48795a6b9eeb9b93d7fec0083674d73e78be1129
Will Fairclough <will@willdoes.dev>
2024-10-07 20:48:40 -0400

    Refactor voting presentation component and add welcome slide
    - Refactor the voting presentation component to improve code readability and maintainability.
    - Add a welcome slide to the presentation to provide an introduction to the SpooktoberFest event.

--- packages/spooktoberfest-remix/app/routes/voting-presentation.tsx
+++ packages/spooktoberfest-remix/app/routes/voting-presentation.tsx
@@ -1,32 +1,56 @@
 import { Slide } from "~/components/slides/slide";
-import { NodeMouseHandler, ReactFlow, ReactFlowProvider, useReactFlow } from "@xyflow/react";
-
+import {
+  NodeMouseHandler,
+  ReactFlow,
+  ReactFlowProvider,
+  useReactFlow,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+
+import bgAudio from "~/assets/audio/vote-for-your-nightmare.mp3";
 import { MovieSlide } from "~/components/slides/movie-slide";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { createRef, useCallback, useEffect, useState } from "react";
 import { MovieNomination } from "~/models/movies";
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { globalMoviesService } from "~/services/movies.service";
 import { globalCacheService } from "~/services/cache.service";
 import { useLoaderData } from "@remix-run/react";
+import { WelcomeSlide } from "~/components/slides/welcome-slide";
 
 const nodeTypes = {
   slide: Slide,
   movieSlide: MovieSlide,
+  welcomeSlide: WelcomeSlide,
+};
+
+const mainStyle = {
+  'gridTemplateColumns': '5rem 1fr 5rem',
 };
 
 export interface VotingPresentationProps {
-  nominations: MovieNomination[]
+  nominations: MovieNomination[];
 }
 
 const VotingPresentation = ({ nominations }: VotingPresentationProps) => {
+  const audRef = createRef<HTMLAudioElement>();
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
   const { fitView } = useReactFlow();
+
+  useEffect(() => {
+    const audio = audRef.current;
+    if (!audio) return;
+    if (isPlaying) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }, [isPlaying]);
 
   const nodes = [
     {
       id: "0",
-      type: "slide",
+      type: "welcomeSlide",
       position: { x: 0, y: 0 },
       data: {
         source: `
@@ -37,7 +61,7 @@ const VotingPresentation = ({ nominations }: VotingPresentationProps) => {
       },
     },
     ...nominations.map((nom, i) => ({
-      id: (i + 1) + "",
+      id: i + 1 + "",
       type: "movieSlide",
       position: { x: 3000 * (i + 1), y: 0 },
       data: { movieId: nom.id, nominees: nom.nominees },
@@ -71,6 +95,7 @@ const VotingPresentation = ({ nominations }: VotingPresentationProps) => {
 
   const handleNodeClick = useCallback<NodeMouseHandler>(
     (_, node) => {
+      setIsPlaying(true);
       const currId = parseInt(node.id);
       const nextNode = nodes.find((n) => parseInt(n.id) > currId);
       if (!nextNode) return;
@@ -79,9 +104,33 @@ const VotingPresentation = ({ nominations }: VotingPresentationProps) => {
     [fitView],
   );
 
+  const handleNextSlide = useCallback(() => {
+    const nextSlide = Math.min(currentSlide + 1, nodes.length - 1);
+    const nextNode = nodes.find((n) => parseInt(n.id) === nextSlide);
+    if (!nextNode) return;
+    fitView({ nodes: [nextNode], duration: 250 });
+    setCurrentSlide(nextSlide);
+  }, [currentSlide, nodes, fitView]);
+
+  const handlePrevSlide = useCallback(() => {
+    const nextSlide = Math.max(currentSlide - 1, 0);
+    const prevNode = nodes.find((n) => parseInt(n.id) === nextSlide);
+    if (!prevNode) return;
+    fitView({ nodes: [prevNode], duration: 250 });
+    setCurrentSlide(nextSlide);
+  }, [currentSlide, nodes, fitView]);
+
   return (
     <>
-      <main className="dark w-[100vw] h-[100dvh]">
+      <audio ref={audRef}>
+        <source src={bgAudio} type="audio/mpeg" />
+        <track kind="captions" />
+        Your browser does not support the audio element.
+      </audio>
+      <main className="dark w-[100vw] h-[100dvh] grid" style={mainStyle}>
+        <button onClick={handlePrevSlide}>
+          {currentSlide > 1 ? '<' : ''}
+        </button>
         <ReactFlow
           nodes={nodes}
           nodeTypes={nodeTypes}
@@ -93,17 +142,18 @@ const VotingPresentation = ({ nominations }: VotingPresentationProps) => {
           fitViewOptions={{ nodes: [{ id: currentSlide + "" }] }}
           onNodeClick={handleNodeClick}
         />
-        ;
+        <button onClick={handleNextSlide}>
+          {currentSlide > 0 && currentSlide < nodes.length - 1 ? '>' : ''}
+        </button>
       </main>
     </>
   );
 };
 
-
 export async function loader({ request }: LoaderFunctionArgs) {
   const nomsKeys = await globalCacheService.keys("nominations:*");
   const jsonStrValues = await globalCacheService.mGet(nomsKeys);
-  const values = jsonStrValues.map((v) => v && JSON.parse(v) as unknown);
+  const values = jsonStrValues.map((v) => v && (JSON.parse(v) as unknown));
   const nominations: MovieNomination[] = [];
   const movieIds = new Set<number>();
   const nomineesMap = new Map<number, string[]>();
@@ -142,4 +192,3 @@ export default () => {
     </ReactFlowProvider>
   );
 };
-
