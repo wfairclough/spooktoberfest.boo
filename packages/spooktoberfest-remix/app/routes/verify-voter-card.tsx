40a00dc841c8ee7fcf7a0fbdfd1dac41cac99e71
Will Fairclough <will@willdoes.dev>
2024-10-12 18:42:35 -0400

    Error when link is invalid

--- packages/spooktoberfest-remix/app/routes/verify-voter-card.tsx
+++ packages/spooktoberfest-remix/app/routes/verify-voter-card.tsx
@@ -6,14 +6,22 @@ const isProd = process.env.SPK_ENV === "prod";
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const search = new URL(request.url).searchParams;
   const jwt = search.get("voter_card") as string;
-  const voterCard = await verifyVoterCard(jwt);
-  return redirect(`/vote?name=${voterCard.name}`, {
-    headers: isProd
-      ? {
-        "Set-Cookie": `voter_card=${jwt}; Path=/; HttpOnly; Secure; SameSite`,
-      }
-      : {
-        "Set-Cookie": `voter_card=${jwt}; Path=/; HttpOnly; SameSite`,
-      },
-  });
+  try {
+    const voterCard = await verifyVoterCard(jwt);
+    return redirect(`/vote?name=${voterCard.name}`, {
+      headers: isProd
+        ? {
+            "Set-Cookie": `voter_card=${jwt}; Path=/; HttpOnly; Secure; SameSite`,
+          }
+        : {
+            "Set-Cookie": `voter_card=${jwt}; Path=/; HttpOnly; SameSite`,
+          },
+    });
+  } catch (error) {
+    return redirect(
+      `/get-voter-card?error=${encodeURIComponent(
+        "It seems your voter card is invalid. Maybe it expired. Please try again.",
+      )}`,
+    );
+  }
 };
